bdfc3f8f1cee5b88111db26294b3d08233b3f6ca
Manuel Alvarado <manualvaradoibarr@gmail.com>
2024-09-13 12:52:56 -0600

    Connect Python to MongoDB Collection (url field) and obtain the data

--- scrapping.py
+++ scrapping.py
@@ -2,41 +2,43 @@ import requests
 from bs4 import BeautifulSoup
 from pymongo import MongoClient
 
-websitesLists = [
-    "https://centralgps.com.mx/", 
-    "https://www.lala.com.mx/", 
-]
-
+#Esta función recibe una URL y un nombre de archivo, y guarda el texto extraído de la URL en el archivo.	
 def fetch_and_extract_text(url, filename):
     response = requests.get(url)
     
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, 'html.parser')
 
+        #Este código elimina los scripts y estilos de la página.
         for element in soup(["script", "style"]):
           element.extract()
         extracted_text = soup.get_text()
 
         extracted_text = ' '.join(extracted_text.split())
+
+        #Aqui se guarda el texto extraído en un archivo.
         with open(filename, "w", encoding="utf-8") as file:
             file.write(extracted_text)
         return extracted_text
     else:
         return f"Error: No puede obtenerse datos de: {url}"
     
-def print_mongo_values(): 
+#Esta función obtiene las URLs de la base de datos de MongoDB.
+def get_websites_from_mongo(): 
     client = MongoClient("mongodb://localhost:27017/")
-    db = client["websiteLists"]
-    collection = db["websites"]
-    for website in collection.find():
-        print(website)
+    db = client["websiteLists"] #Mi base de datos
+    collection = db["websites"] #Mi colección
+
+    urls = []
+    for document in collection.find():
+        urls.append(document["url"])
+    return urls
 
 if __name__ == "__main__":
-    for index, url in enumerate(websitesLists):
+
+    urls = get_websites_from_mongo()
+    for index, url in enumerate(urls):
         print(f"Obteniendo el HTML de: {url}")
         filename = f"extracted_text_{index+1}.txt"
         extracted_text = fetch_and_extract_text(url, filename)
         print("Texto extraído:")
-
-    print("Valores de la base de datos:")
-    print_mongo_values()
