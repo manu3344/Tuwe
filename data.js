d3e3c234055bf4f106968cdc9eb52a0da0b251b5
Manuel Alvarado <manualvaradoibarr@gmail.com>
2024-09-13 13:24:38 -0600

    Add data to insert

---
+++ data.js
@@ -0,0 +1,17 @@
+[
+  {
+    "nombre": "Central GPS",
+    "url": "https://centralgps.com.mx/",
+    "text": ""
+  }, 
+  {
+    "nombre": "Syscam",
+    "url": "https://www.syscam.mx/",
+    "text": ""
+  }, 
+  {
+    "nombre": "Lala", 
+    "url": "https://www.lala.com.mx/",
+    "text": "https://www.lala.com.mx/"
+  }
+]
