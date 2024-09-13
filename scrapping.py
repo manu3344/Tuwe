adb1aa940a962753f11196d815ad7a8e98a6d6da
Manuel Alvarado <manualvaradoibarr@gmail.com>
2024-09-13 13:14:10 -0600

    Extract the url field of each document, and extract the content to save it in the text field in Mongo

--- scrapping.py
+++ scrapping.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 from pymongo import MongoClient
 
 #Esta función recibe una URL y un nombre de archivo, y guarda el texto extraído de la URL en el archivo.	
-def fetch_and_extract_text(url, filename):
+def fetch_and_extract_text(url):
     response = requests.get(url)
     
     if response.status_code == 200:
@@ -15,10 +15,6 @@ def fetch_and_extract_text(url, filename):
         extracted_text = soup.get_text()
 
         extracted_text = ' '.join(extracted_text.split())
-
-        #Aqui se guarda el texto extraído en un archivo.
-        with open(filename, "w", encoding="utf-8") as file:
-            file.write(extracted_text)
         return extracted_text
     else:
         return f"Error: No puede obtenerse datos de: {url}"
@@ -29,16 +25,31 @@ def get_websites_from_mongo():
     db = client["websiteLists"] #Mi base de datos
     collection = db["websites"] #Mi colección
 
-    urls = []
-    for document in collection.find():
-        urls.append(document["url"])
-    return urls
+    return list(collection.find())
+
+def save_text_to_mongo(document_id, text):
+    client = MongoClient("mongodb://localhost:27017/")
+    db = client["websiteLists"]
+    collection = db["websites"]
+    collection.update_one({"_id": document_id}, {"$set": {"text": text}})
 
 if __name__ == "__main__":
 
-    urls = get_websites_from_mongo()
-    for index, url in enumerate(urls):
-        print(f"Obteniendo el HTML de: {url}")
+    documents = get_websites_from_mongo()
+
+    for index, document in enumerate(documents):
+        url = document["url"]
+        print(f"Obteniendo el texto de: {url}")
+        extracted_text = fetch_and_extract_text(url)
+
         filename = f"extracted_text_{index+1}.txt"
-        extracted_text = fetch_and_extract_text(url, filename)
-        print("Texto extraído:")
+        with open(filename, "w", encoding="UTF-8") as file:
+            file.write(extracted_text)
+
+        print(f"Guardando el texto extraído en: {filename}")
+
+        save_text_to_mongo(document["_id"], extracted_text)
+        print(f"Guardando el texto extraído en MongoDB para el documento: {document["nombre"]}")
+
+
+
